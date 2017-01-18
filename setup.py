513821031c9abff0e212db0a10a889c68e280683
schneefux <schneefux+commit@schneefux.xyz>
2017-01-18 15:04:08 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@
 from setuptools import setup
 
 setup(
-    name="Gamelocker-API",
+    name="python-gamelocker",
     version="0.1",
-    description="Gamelocker API Wrapper",
+    description="Python Gamelocker API Wrapper",
     author="schneefux",
-    py_modules=["gamelocker"],
+    author_email="schneefux+pypi_schneefux@schneefux.xyz",
+    url="https://git.schneefux.xyz/schneefux/python-gamelocker",
+    packages=["gamelocker"],
+    install_requires=["requests"],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"]
 )
