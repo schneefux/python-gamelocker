2394e44ab0fc35c2166f9d31681a2dabeefbb7c8
schneefux <schneefux+commit@schneefux.xyz>
2017-03-19 15:19:42 +0100

    bump pip version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="python-gamelocker",
-    version="0.4.0",
+    version="0.5.0",
     description="Python Gamelocker API Wrapper",
     author="schneefux",
     author_email="schneefux+pypi_schneefux@schneefux.xyz",
